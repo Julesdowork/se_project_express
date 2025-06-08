50b945162a48ea525a3c5096cb6974f84b302c6f
Julian McNeill <julesdowork@gmail.com>
2025-06-08 18:53:47 -0400

    Add crash test route & domain name

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # WTWR (What to Wear): Back End
 
+## Live Site
+
+https://www.wtwr.auraria.org/
+
 ## Description
 
 This back-end project is focused on creating a server for my WTWR application. I gained a deeper understanding of how to work with databases, set up security and testing, and deploy web applications on a remote machine. My eventual goal is to create a server with an API and user authorization.
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ const { createUser, login } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
+// TODO: remove after passing code review
+router.use("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now.");
+  }, 0);
+});
 router.use("/signin", login);
 router.use("/signup", createUser);
 router.use((req, res) => {
